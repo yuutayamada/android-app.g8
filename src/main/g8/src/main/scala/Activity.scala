56c8271d9b1192de03ef87fbd6fd2b761e3fc215
Nathan Hamblen <nathan@technically.us>
2011-04-09 12:15:09 -0400

    parametarize main activity

--- src/main/g8/src/main/scala/Activity.scala
+++ src/main/g8/src/main/scala/Activity.scala
@@ -4,7 +4,7 @@ import _root_.android.app.Activity
 import _root_.android.os.Bundle
 import _root_.android.widget.TextView
 
-class MainActivity extends Activity {
+class $main_activity$ extends Activity {
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
     setContentView(new TextView(this) {
