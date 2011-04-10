8d06054c065a01b4cd7027e8988410969415d8be
Nathan Hamblen <nathan@technically.us>
2011-04-10 10:15:51 -0400

    scalatest 1.3

--- src/main/g8/project/build/project.scala
+++ src/main/g8/project/build/project.scala
@@ -5,13 +5,14 @@ trait Defaults {
 }
 class Parent(info: ProjectInfo) extends ParentProject(info) {
   override def shouldCheckOutputDirectories = false
+  override def updateAction = task { None }
 
   lazy val main  = project(".", "$name$", new MainProject(_))
   lazy val tests = project("tests",  "tests", new TestProject(_), main)
 
   class MainProject(info: ProjectInfo) extends AndroidProject(info) with Defaults with MarketPublish with TypedResources {
     val keyalias  = "change-me"
-    val scalatest = "org.scalatest" % "scalatest" % "1.0" % "test"
+    val scalatest = "org.scalatest" % "scalatest" % "1.3" % "test"
   }
 
   class TestProject(info: ProjectInfo) extends AndroidTestProject(info) with Defaults
