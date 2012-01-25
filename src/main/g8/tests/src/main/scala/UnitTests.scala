ed5755794034b4c3fdf3dab3b05b408967376fbd
Jan Berkel <jan.berkel@gmail.com>
2012-01-25 09:58:12 +0100

    Add instrumentation test

--- src/main/g8/tests/src/main/scala/UnitTests.scala
+++ src/main/g8/tests/src/main/scala/UnitTests.scala
@@ -2,9 +2,18 @@ package $package$.tests
 
 import junit.framework.Assert._
 import _root_.android.test.AndroidTestCase
+import _root_.android.test.ActivityInstrumentationTestCase2
 
 class UnitTests extends AndroidTestCase {
   def testPackageIsCorrect {
     assertEquals("$package$", getContext.getPackageName)
   }
+}
+
+class ActivityTest extends ActivityInstrumentationTestCase2(classOf[$main_activity$]) {
+   def testHelloWorldIsShown() {
+      val activity = getActivity()
+      val textview = activity.findView($package$.TR.textview)
+      assertEquals(textview.getText(), "hello, world!")
+    }
 }
