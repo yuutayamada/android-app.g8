cb4a3284ed4c5deb7954db032ee93a893873941f
Jan Berkel <jan.berkel@gmail.com>
2012-01-25 10:04:59 +0100

    Fix imports

--- src/main/g8/tests/src/main/scala/Tests.scala
+++ src/main/g8/tests/src/main/scala/Tests.scala
@@ -1,19 +1,20 @@
 package $package$.tests
 
+import $package$._
 import junit.framework.Assert._
 import _root_.android.test.AndroidTestCase
 import _root_.android.test.ActivityInstrumentationTestCase2
 
-class UnitTests extends AndroidTestCase {
+class AndroidTests extends AndroidTestCase {
   def testPackageIsCorrect {
     assertEquals("$package$", getContext.getPackageName)
   }
 }
 
-class ActivityTest extends ActivityInstrumentationTestCase2(classOf[$main_activity$]) {
+class ActivityTests extends ActivityInstrumentationTestCase2(classOf[$main_activity$]) {
    def testHelloWorldIsShown() {
       val activity = getActivity()
-      val textview = activity.findView($package$.TR.textview)
+      val textview = activity.findView(TR.textview)
       assertEquals(textview.getText(), "hello, world!")
     }
 }
